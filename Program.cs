42eead5215562fde68a9a491c793f5e97bae0b56
alexk <alexk@DESKTOP-QL7PNT4>
2024-06-04 09:45:45 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Reflection.Metadata;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Assignment_5._1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userString = "121";
+
+
+            Console.WriteLine(IsPalindrome(userString));
+
+            bool IsPalindrome(string input) 
+            {
+                int L = 0;
+                int R = input.Length - 1;
+
+                while (L < R)
+                {
+                    if (userString[0] == userString[R])
+                    {
+                        L++;
+                        R--;
+                    }
+                    else 
+                    {
+                        return false;
+                    } 
+                }
+                return true;
+            }      
+                
+                
+                
+                    
+                
+
+            
+            
+            
+
+        }
+    }
+}
